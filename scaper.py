5d518fde291515607472885a2755eaab3b7f633b
naim08 <mmiah@fordham.edu>
2016-10-19 10:51:31 -0400

    maybe my only commit

---
+++ scaper.py
@@ -0,0 +1,37 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys 
+import math
+import random
+
+def constantTimer(min, max):
+	return math.floor(random.randint(min,max)) + min
+
+browser = webdriver.PhantomJS('/Users/Naim/Desktop/finanicalprogramming/scrapetweets/phantomjs-2.1.1-macosx/bin/phantomjs')
+query = u'remicade'
+base_url = u'https://twitter.com/search?f=tweets&vertical=news&q='+ query +'&src=typd'
+url = base_url + query
+
+browser.get(url)
+
+body = browser.find_element_by_tag_name('body')
+
+lastHeight = browser.execute_script("return document.body.scrollHeight")
+
+for _ in range(3):
+	browser.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+	timer = constantTimer(1,3)
+	print timer
+	time.sleep(timer)
+	newHeight = browser.execute_script("return document.body.scrollHeight")
+	print newHeight
+	if newHeight == lastHeight:
+		break
+	lastHeight = newHeight
+
+tweets = browser.find_elements_by_class_name('tweet-text')
+
+for tweet in tweets:
+	print(tweet.text)
+
+browser.quit()
